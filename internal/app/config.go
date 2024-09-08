8fc1b56a15bca06f2e09d3b63ac906775fe95e64
Duc Nguyen <minhduc140583@gmail.com>
2024-09-08 13:50:33 +0700

    Refactor code

--- internal/app/config.go
+++ internal/app/config.go
@@ -12,6 +12,7 @@ type Config struct {
 	Log        log.Config          `mapstructure:"log"`
 	MiddleWare gin.LogConfig       `mapstructure:"middleware"`
 }
+
 type SqlConfig struct {
 	DataSourceName string `yaml:"data_source_name" mapstructure:"data_source_name" json:"dataSourceName,omitempty" gorm:"column:datasourcename" bson:"dataSourceName,omitempty" dynamodbav:"dataSourceName,omitempty" firestore:"dataSourceName,omitempty"`
 	Driver         string `yaml:"driver" mapstructure:"driver" json:"driver,omitempty" gorm:"column:driver" bson:"driver,omitempty" dynamodbav:"driver,omitempty" firestore:"driver,omitempty"`
